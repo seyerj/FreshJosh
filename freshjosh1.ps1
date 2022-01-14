726db907c779f9ad5874c8050d1c069625f6a83e
Josh Seyer <seyerj@users.noreply.github.com>
2022-01-14 08:19:30 -0800

    Updated Chocolatey install command to current

--- freshjosh1.ps1
+++ freshjosh1.ps1
@@ -4,7 +4,7 @@
 
 
 #Installs Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install 7zip
 choco install 7zip.install -y
